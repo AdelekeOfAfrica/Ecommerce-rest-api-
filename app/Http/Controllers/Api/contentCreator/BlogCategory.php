379af3b70e95ffef0badf4d51c448ca9f9bc22c7
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2023-04-25 13:03:09 +0100

    added the schemas for blog and blogcategory, and also added their controllers

---
+++ app/Http/Controllers/Api/contentCreator/BlogCategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\contentCreator;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BlogCategory extends Controller
+{
+    //
+}
---
+++ database/migrations/2023_04_25_114415_create_blog_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\BlogCategory;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignIdFor(BlogCategory::class)->constrained();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+};
